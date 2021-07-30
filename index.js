dca2a05522fd009cdcac0c4e896eebb970d907eb
jungkiju <thek226@gmail.com>
2021-07-30 17:04:28 +0900

    routing

--- index.js
+++ index.js
@@ -29,9 +29,18 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+  } else if (req.url === '/users') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('User list');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
 // listen() 서버는 요청대기상태로 만들어 주는 함수
