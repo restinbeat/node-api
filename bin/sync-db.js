8540c5ec1f7066d1f0276f5f6ed8f8e54cbb9a03
jungkiju <thek226@gmail.com>
2021-08-26 17:53:31 +0900

    Update sqlite

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -1,5 +1,8 @@
 const models = require('../models');
 
 module.exports = () => {
-  return models.sequelize.sync({force: true}); // force: true 기존의 DB가 있더라도 빈 큐로 만듬
+  const options = {
+    force: process.env.NODE_ENV === 'test' ? true : false
+  }
+  return models.sequelize.sync(options); // force: true 기존의 DB가 있더라도 빈 큐로 만듬
 }
